d9d481a8f4b1bf3c03dda5b830ed8122404962a0
Ku Young Shin <ky2shin@uwaterloo.ca>
2020-04-19 21:33:41 -0400

    Adjust predictions with python

--- scripts/adjust_prediction.py
+++ scripts/adjust_prediction.py
@@ -3,15 +3,17 @@ import itertools
 def calc_weight(num):
     return (1001-num) / 100000
 
-def calc_accuracy(prediction):
+def calc_accuracy(prediction, print_values=False):
     actual = [1469, 2088, 2790, 3251, 4042, 4682, 5576, 6280, 7398, 8527, 9560, 11284, 12437, 12978, 15756, 16563, 17872, 19141, 20654, 22059, 23316, 24298, 25679, 27034, 28208, 30808, 32813, 34355]
     delta = [340.6661140000001, 167.75270200000023, 16.712113000000045, 114.82030600000007, 6.138746000000083, 102.8047459999998, 36.88873500000045, 239.19654400000036, 103.69540700000016, 30.30747800000063, 122.00756200000069, 413.0587400000004, 318.4429199999995, 440.74631000000045, 991.0149600000004, 412.5266100000008, 303.73219999999856, 129.59659000000102, 180.9960600000013, 112.62207000000126, 109.10023000000001, 605.0789399999994, 695.6079700000009, 800.4060099999988, 1069.6430900000014, 108.04416000000128, 715.5471499999985, 888.2883999999976]
     count = 0
     for x,y,d in zip(actual, prediction, delta):
         if abs(x-y) < d:
+            if print_values:
+                print('Better value: {}'.format(y))
             count += 1
 
-    print('Count: {}, Percentage: {}'.format(count, count/len(actual)))
+    # print('Count: {}, Percentage: {}'.format(count, count/len(actual)))
     return count, count/len(actual)
 
 sign = [1001, 1001, 1001, 1001, 348, 45, 32, 91, 165, 49, 41, 62, 36, 91, 53, 77, 55, 84, 71, 104, 48, 59, 81, 81, 119, 90, 169, 84, 121, 90, 137, 248, 203, 228, 258]
@@ -23,15 +25,20 @@ predict = [1809.666114, 2255.752702, 2773.287887, 3365.820306, 4035.861254, 4784
 
 nums = [1,2,3,4,5]
 adjustment_records = []
-for a,b,c,d in itertools.permutations(nums,4):
-    print('a: {}, b: {}, c: {}, d: {}'.format(a,b,c,d))
+for a,b,c,d in itertools.product(range(1,6),repeat=4):
+    # print('a: {}, b: {}, c: {}, d: {}'.format(a,b,c,d))
     adjustments = []
     for si, st, he, co, pr in zip(sign, stay, herd, contact, predict):
         multiplier = 1 - a*calc_weight(si) - b*calc_weight(st) + c*calc_weight(he) - d*calc_weight(co)
         adjustments.append(pr*multiplier)
-        print(pr*multiplier)
-    count, percentage = calc_accuracy(adjustments)
+        if (a == 1 and b == 1 and c == 5 and d == 4):
+            print(pr*multiplier)
+    if (a == 1 and b == 1 and c == 5 and d == 4):
+        count, percentage = calc_accuracy(adjustments, print_values=True)
+    else:
+        count, percentage = calc_accuracy(adjustments)
     adjustment_records.append(((a,b,c,d), percentage))
 
 adjustment_records.sort(key=lambda x:x[1], reverse=True)
-print(adjustment_records)
+for item in adjustment_records[:10]:
+    print(item)
